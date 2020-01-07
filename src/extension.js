b8b16404fa27e25f1eb6c4411ee314c9f4897bf4
Martin Mouterde <martin.mouterde@gmail.com>
2020-01-07 17:08:43 +0100

    change implementation to get FF&Chrome iso-code

--- src/extension.js
+++ src/extension.js
@@ -1,19 +1,17 @@
 function init() {
-    const _setData = DataTransfer.prototype.setData;
-    DataTransfer.prototype.setData = function (type, value) {
-        if ((type === "text/plain" || type === "text")) {
-            const selection = window.getSelection().toString();
-            if (selection === value) {
-                _setData.apply(this, [type, value]);
-            } else {
-                const answer = confirm(`SaferClipboard notices that you have just copied something different from your selection.\n\nDo you want to proceed anyway ?\n\nCopied value : ${value}.`);
-                if (answer) {
-                    _setData.apply(this, [type, value]);
+    window.document.addEventListener('copy', () => {
+        setTimeout(async () => {
+            try {
+                const clipText = await navigator.clipboard.readText();
+                const selection = window.getSelection().toString();
+                if (selection !== clipText) {
+                    const answer = confirm(`SaferClipboard : you have just copied something different from your selection.\n\nDo you want to do the real copy instead ?\n\nCopied value : ${clipText}.`);
+                    if (answer) {
+                        setTimeout(async () => navigator.clipboard.writeText(selection));
+                    }
                 }
-            }
-        } else {
-            _setData.apply(this, [type, value]);
-        }
-    };
+            }catch(err){console.error(err);}
+        });
+    }, true);
 }
 init();
