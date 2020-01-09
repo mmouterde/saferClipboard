d6117e20721f098e29c32db1a5df0910953fd6ca
Martin Mouterde <martin.mouterde@gmail.com>
2020-01-09 13:28:28 +0100

    Add trim Avoid check if selection is empty 1.0.2

--- src/extension.js
+++ src/extension.js
@@ -1,17 +1,20 @@
 function init() {
     window.document.addEventListener('copy', () => {
+        const selection = document.getSelection().toString().trim();
         setTimeout(async () => {
             try {
-                const clipText = await navigator.clipboard.readText();
-                const selection = window.getSelection().toString();
-                if (selection !== clipText) {
+                const clipText = await navigator.clipboard.readText().trim();
+                if (selection.length > 0 && selection !== clipText) {
                     const answer = confirm(`SaferClipboard : you have just copied something different from your selection.\n\nDo you want to do the real copy instead ?\n\nCopied value : ${clipText}.`);
                     if (answer) {
                         setTimeout(async () => navigator.clipboard.writeText(selection));
                     }
                 }
-            }catch(err){console.error(err);}
+            } catch (err) {
+                console.error(err);
+            }
         });
     }, true);
 }
+
 init();
